cf0c72a040c21e20cef2275c5152975a2022e23f
Theo Jolliffe <theojolliffe@gmail.com>
2022-02-05 16:29:47 +0000

    commit

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -8,7 +8,13 @@
 
     $: console.log('slug', slug)
 
-    var domain = "http://localhost:3000"
+    var domain
+
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+        domain = "http://localhost:3000"
+    } else {
+        domain = "https://occupation.vercel.app"
+    }
 
     let expanded = null;
     function toggle(code) {
