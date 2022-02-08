9e74065af787ce492a2b53def1a117cfb081ea34
Theo Jolliffe <theojolliffe@gmail.com>
2022-02-08 11:07:38 +0000

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import dsv from '@rollup/plugin-dsv';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		vite: {
+			plugins: [
+				dsv()
+			]
+		}
 	}
 };
 
